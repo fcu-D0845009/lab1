6cad77a1ef6b758e68764632e2d34e19febefd94
user <simon99131@gmail.com>
2020-09-29 09:33:00 +0800

    作業

--- src/lab11/lab111.java
+++ src/lab11/lab111.java
@@ -3,6 +3,8 @@ package lab11;
 public class lab111 {
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World OOP");
+		System.out.println("I'm ©x¤lµ¾");
+		System.out.println("My nid is D0845009");
+		System.out.println("This is lab 1 hw");
 	}
 }
